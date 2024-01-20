d2b961f908f9b35bb043da34ed648203fe239bd6
pc-v2 <meguminxkazuma@protonmail.com>
2024-01-20 23:58:36 +0700

    add v3-ansi

--- keyboards/keychron/v3/ansi/rules.mk
+++ keyboards/keychron/v3/ansi/rules.mk
@@ -21,7 +21,12 @@ RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = CKLED2001
 EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = embedded_flash
+OPENRGB_ENABLE = yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
+# custom matrix setup
+CUSTOM_MATRIX = lite
+
+SRC += matrix.c
